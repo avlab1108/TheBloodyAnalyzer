c98ac93a9eeaf2cbbbbac8562773279c8bc42afe
rsmmnt <rusmamont@gmail.com>
2016-02-01 19:04:26 +0300

    fucked all up, changed scripts

--- positionFinder.py
+++ positionFinder.py
@@ -8,16 +8,20 @@ import glob
 import fnmatch
 from subprocess import call
 
-numParticles = "128"
-path = "/home/dmitry/NAS/linear/" + numParticles + "/globule/LAMMPS"
-Parameters = numParticles + " 1.5 0.2"
+
+
+numParticles = sys.argv[1]
+path = sys.argv[2]
+Parameters = numParticles + " "+ sys.argv[3] + " " + sys.argv[4]
+print  (Parameters)
 fnames = []
 f2 = open("filenames.txt","w")
 for root, dirs, files in os.walk(path):
     for basename in files:
-        if fnmatch.fnmatch(basename, 'posit*'):
+        if fnmatch.fnmatch(basename, 'posit*.txt'):
             filename = os.path.join(root, basename)
             fnames.append(filename)
+            print(filename)
 strng = ""
 strng = str(len(fnames))
 f2.write(strng + "\n")
