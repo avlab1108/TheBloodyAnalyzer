14ea87a580966931cb6e1fd3abeeeb4288ea1555
rsmmnt <rusmamont@gmail.com>
2016-02-01 19:07:10 +0300

    fucked all up, changed scripts

---
+++ DoFgAll.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Argument with folder required'
+    exit 1
+fi
+
+AWKCOMMAND="awk -f $PWD/convlmppos.awk unwrappedDump-2500000.txt"
+
+cd $1
+
+for i in {1..104..1}
+do
+dname="run-"$i
+
+cd $dname
+echo $dname
+$AWKCOMMAND > tmpp.txt
+sort -k1 --numeric tmpp.txt | awk '{
+print  $2," ",$3," ",$4," ",$5," 0 0 0" ;
+
+}' > position.txt
+
+cd ..
+done
+
+
+python positionFinder.py $2 $1 1.2 1
