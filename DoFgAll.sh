9a7b6788378d5089a3b1e7f1cd1068a7e12274bb
pozharko <canoneer@mail.ru>
2016-03-14 09:35:17 +0300

    Use last available dump

--- DoFgAll.sh
+++ DoFgAll.sh
@@ -7,29 +7,33 @@ fi
 
 HOME=$(pwd)
 DATA_PATH=$1
-AWKCOMMAND="awk -f $HOME/convlmppos.awk dump.3000000.txt"
 
 cd $DATA_PATH
-LIMIT=100
+LIMIT=200
 rm -f "$HOME/filenames.txt"
 COUNT=0
-POSITIONFILE="position_T_04_30.txt"
-TEMP=04
+POSITIONFILE="position.txt"
+TEMP="stats-1.4"
 
 for (( i=1; i<=$LIMIT; i++ ))
 do
     DNAME="run-$i"
     if [ -d $DNAME ]; then
-        COUNT=`expr $COUNT + 1`
         cd $DNAME
         echo $(pwd)
-#        $AWKCOMMAND > tmpp.txt
-#        sort -k1 --numeric tmpp.txt | awk '{
-#        print  $2," ",$3," ",$4," ",$5," 0 0 0" ;
-#
-#        }' > position.txt
 
-       [ -f $DATA_PATH/$DNAME/$POSITIONFILE ] && echo "$DATA_PATH/$DNAME/$POSITIONFILE " >> "$HOME/filenames.txt"
+        if [ ! -f $DATA_PATH/$DNAME/$POSITIONFILE ]; then
+            LAST_DUMP=$(ls -la dump*.txt | sort -k7 | tail -1 | awk '{print $9;}' )
+            awk -f $HOME/convlmppos.awk $LAST_DUMP > tmpp.txt
+            sort -k1 --numeric tmpp.txt | awk '{
+            print  $2," ",$3," ",$4," ",$5," 0 0 0" ;
+            }' > position.txt
+        fi
+
+        if [ -f $DATA_PATH/$DNAME/$POSITIONFILE ]; then
+           COUNT=`expr $COUNT + 1`
+           echo "$DATA_PATH/$DNAME/$POSITIONFILE " >> "$HOME/filenames.txt"
+        fi
         cd ..
     fi
 done
@@ -40,7 +44,7 @@ head "$HOME/filenames.txt"
 
 cd $HOME
 
-./analyzer.exe $2 1.2 0.2
+./analyzer.exe $2 1.4 10.0
 
 echo "plot graph"
 echo $HOME
